0c84d7d38d1c64f9ce136af04b6cde3383f40a01
Christian <118530836+faze-chris@users.noreply.github.com>
2024-09-23 14:04:57 +0200

    challenge done SYNRGY-14
    find the duplicate number I tested it in leet code

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 console.log("hello world");
 
+const nums = [1, 2, 3, 4, 2];  // number array
+
+function findDuplicate(nums) {
+    let A = nums[0];
+    let B = nums[0];
+
+    
+    do {
+        A = nums[A];
+        B = nums[nums[B]];
+    } while (A !== B);
+
+
+    A = nums[0];  
+
+    while (A !== B) {
+        A = nums[A];
+        B = nums[B];
+    }
+
+    return A;
+}
+
+// print result 
+console.log("Duplicate number is:", findDuplicate(nums));
