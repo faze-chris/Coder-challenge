22f67e6ffc9cc14f67fa58f165e0da0f98c4f3a8
Christian <118530836+faze-chris@users.noreply.github.com>
2024-09-23 16:26:41 +0200

    challenge done SYNRGY-14
    two sums done i tested in leet code but i got a error again but i did what was ask of the challenge and it work in vs code

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 console.log("hello world");
 
+const nums = [2,7,11,15]; // number array
+const target = 9;  // target sum
+
+function numbers(nums, target) {
+    const numb = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const number = target - nums[i];
+        if (number in numb) {
+            return [numb[number], i];
+        }
+        numb[nums[i]] = i;
+    }
+}
+
+//consol log result
+console.log(numbers(nums, target));
